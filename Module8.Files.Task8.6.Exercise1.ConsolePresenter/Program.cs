bcca8f4528b07772fe589766d30b5b8a98fa2d76
Vasilev-VV <vasiavasilev97@mail.ru>
2024-11-12 21:30:17 +0700

    Инициализация проекта

---
+++ Module8.Files.Task8.6.Exercise1.ConsolePresenter/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Module8.Files.Task8._6.Exercise1.ConsolePresenter;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
